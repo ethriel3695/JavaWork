6db6a60c906b2ef89517bdc72a3830ba989a9ca1
Reuben Ellis <ethriel3695@gmail.com>
2016-08-13 23:14:09 -0600

    Atomic Concurrency Finished
    The program is complete and working properly

--- AtomicConcurrency/src/atomicmethodconcurrency/AtomicConcurrency.java
+++ AtomicConcurrency/src/atomicmethodconcurrency/AtomicConcurrency.java
@@ -1,88 +1,251 @@
-package atomicmethodconcurrency;
-import java.util.List;
-import java.util.concurrent.*;
-import java.util.concurrent.ThreadFactory;
 /*
  * @author Reuben Ellis
  * Date: 2016-08-11
  * This program demonstrates two threads invoking a method concurrently
  * for class PRG/421
- * Write a Java program (non-GUI preferred) that has a method named atomic().
- * Demonstrate in the program how two threads can, sometimes, invoke atomic() concurrently.
- * Create a second version of the program in which the two threads cannot invoke atomic concurrently.
- * Submit both programs using the Assignment Files tab above.
+ * An ArrayList of type Integer is created to hold the random starting streets
+ * for a car
+ 
+ * In the main method, four instances are created for the 
+ * TrafficIntersectionStartingPoint class which is a Callable thread of type 
+ * Integer
+ * Future is then used to initiate each thread and submit a call to the class
+ * which returns the thread name
+ * A try-catch statement is then initiated to catch and Exception that may
+ * happen if there is an error with executing the threads or if the thread is
+ * interrupted
+ * An ArrayList of type Integer then adds each thread name to the list for use
+ * with accessing the ConcurrentHashMap for starting street values
+ * The next step within the main method is to initiate the randomStreet method
+
+ * The randomStreet method contains a for loop which iterates four times, one
+ * for each thread
+ * The streetChoice int variable equals a ThreadLocalRandom value between 
+ * 1 and 4
+ * A check is in place to determine if the threadIndex ArrayList contains one
+ * of the random values already to ensure each thread gets a unique street to 
+ * start on using an if statement and then a while loop which calls the random
+ * action until a unique value is found
+ * Once the statement is successful, the user receives a message indicating
+ * which intersection each car begins at and the thread name and intersection 
+ * number are placed within a ConcurrentHashMap for use in the main method
+
+ * Back in the main method, a variable receives the starting intersection for 
+ * each thread and another random value is initiated
+ * The random value is checked against the current intersection for the current
+ * thread and a while loop is in place to continue getting a new value as
+ * long as the random value matches the current intersection value
+ * Once the random value is different than the intersection value, a check is in
+ * place for each thread besides thread 1
+ * If the thread is thread 1, a message is displayed to the user indicating
+ * which intersection the thread is driving towards
+ * For thread 2, 3, and 4, a check is in place to indicate if the previous
+ * thread is moving from a conflicting intersection
+ * For example, if car 1 is in intersection 2 and heading to intersection 1
+ * and car 2 is intersection 3 and heading to intersection 1, then car 2 must
+ * wait with the use of a Thread.sleep(6000) before driving to the intersection
+ * Each thread besides thread one is checked in this manner and if cars are
+ * travelling parallel such as car 1 is in intersection 1 and going to 3 and
+ * car 2 is in intersection 3 and going to 1 then the cars can go with no issues
+ * but if cars must turn then the current car must wait for the previous car 
+ * to finish crossing the intersection
+ * Once all four threads are complete, the shutdownThreads method is called
+
+ * In the shutdownThreads method, a try-catch-finally block is in place to make
+ * sure the thread is not interrupted if the shutdown process occurs
+ * In the finally portion of the block, if the thread is not terminated, then 
+ * the shutdownNow method of the the ExecutorService is called to shut each
+ * thread down immediately
  */
 
-/**
- *
- * @author ethri
- */
+package atomicmethodconcurrency;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.concurrent.ThreadFactory;
+
 public class AtomicConcurrency {
+
+        
+    private static ArrayList<Integer>
+            threadIndex = new ArrayList<Integer>();
+    
+    private static int
+            streetChoice = 0,
+            keyValue = 0;
+    
+    private static ArrayList<Integer>
+            streetPosition = new ArrayList<Integer>();
+    
+    private static ConcurrentHashMap
+            carInfo = new ConcurrentHashMap();
+    
+    private static ExecutorService
+            streetLocation = Executors.newFixedThreadPool(4);
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TimeoutException {
         
         Callable<Integer> streetStart1 = new TrafficIntersectionStartingPoint();
         Callable<Integer> streetStart2 = new TrafficIntersectionStartingPoint();
         Callable<Integer> streetStart3 = new TrafficIntersectionStartingPoint();
         Callable<Integer> streetStart4 = new TrafficIntersectionStartingPoint();
         
-        Callable<Integer> car1 = new TrafficIntersectionNavigation();
-        Callable<Integer> car2 = new TrafficIntersectionNavigation();
-        Callable<Integer> car3 = new TrafficIntersectionNavigation();
-        Callable<Integer> car4 = new TrafficIntersectionNavigation();
-       
-        ExecutorService 
-                executeCarAction = Executors.newFixedThreadPool(4);
-        ExecutorService
-                streetLocation = Executors.newFixedThreadPool(4);
-        
         Future<Integer> futureStreet1 = streetLocation.submit(streetStart1);
         Future<Integer> futureStreet2 = streetLocation.submit(streetStart2);
         Future<Integer> futureStreet3 = streetLocation.submit(streetStart3);
         Future<Integer> futureStreet4 = streetLocation.submit(streetStart4);
-        Future<Integer> futureAction1 = executeCarAction.submit(car1);
-        Future<Integer> futureAction2 = executeCarAction.submit(car2);
-        Future<Integer> futureAction3 = executeCarAction.submit(car3);
-        Future<Integer> futureAction4 = executeCarAction.submit(car4);
+
         try {
             
-            Integer streetPosition1 = futureStreet1.get();
-            Integer streetPosition2 = futureStreet2.get();
-            Integer streetPosition3 = futureStreet3.get();
-            Integer streetPosition4 = futureStreet4.get();
+            streetPosition.add(futureStreet1.get());
+            streetPosition.add(futureStreet2.get());
+            streetPosition.add(futureStreet3.get());
+            streetPosition.add(futureStreet4.get());
             
-            Integer street1 = futureAction1.get();
-            Integer street2 = futureAction2.get();
-            Integer street3 = futureAction3.get();
-            Integer street4 = futureAction4.get();
+            randomStreet();
             
+            for (int i = 0; i < 4; i += 1)
+            {
+                int
+                    key1 = (int) carInfo.get(streetPosition.get(0)),
+                    key2 = (int) carInfo.get(streetPosition.get(1)),
+                    key3 = (int) carInfo.get(streetPosition.get(2)),
+                    key4 = (int) carInfo.get(streetPosition.get(3)),
+                    currentKey = 0,
+                    carNumber = 0;
+                
+                streetChoice  = ThreadLocalRandom.current().nextInt(1, 5);
+                
+                keyValue = (int) carInfo.get(streetPosition.get(i));
             
-            System.out.println(car1.toString() + "starts on street: " +
-                    streetPosition1  + " and turns on street: " + street1);
+                if (keyValue == streetChoice)
+                {
+                    while (keyValue == streetChoice)
+                    {
+                        streetChoice = ThreadLocalRandom.current().nextInt(1, 5);
+                    }
+                }
+                else
+                {
+                }
+                if (i != 0)
+                {
+                    if (i == 1)
+                    {
+                        currentKey = key1;
+                        carNumber = 0;
+                    }
+                    else if (i == 2)
+                    {
+                        currentKey = key2;
+                        carNumber = 1;
+                    }
+                    else if (i == 3)
+                    {
+                        currentKey = key3;
+                        carNumber = 2;
+                    }
+                    
+                    if ((currentKey == 1 || currentKey == 3) && 
+                            (streetChoice == 3 || streetChoice == 1))
+                    {
+                        System.out.println(String.format("Car %1s drives to"
+                            + " intersection%2s", streetPosition.get(i),
+                            streetChoice));
+                    }
+                    else if ((currentKey == 1 || currentKey == 3) && 
+                            (streetChoice == 2 || streetChoice == 4))
+                    {
+                        System.out.println(String.format("Car %1s must"
+                        + " wait for Car%2s", streetPosition.get(i),
+                        streetPosition.get(carNumber)));
+                        
+                        Thread.sleep(6000);
+                        
+                        System.out.println(String.format("Car %1s drives to"
+                            + " intersection%2s", streetPosition.get(i),
+                            streetChoice));
+                    }
+                    else if ((currentKey == 2 || currentKey == 4) && 
+                            (streetChoice == 2 || streetChoice == 4))
+                    {
+                        System.out.println(String.format("Car %1s drives to"
+                            + " intersection%2s", streetPosition.get(i),
+                            streetChoice));
+                    }
+                    else if ((currentKey == 2 || currentKey == 4) && 
+                        (streetChoice == 1 || streetChoice == 3))
+                    {
+                        System.out.println(String.format("Car %1s must"
+                        + " wait for Car%2s", streetPosition.get(i),
+                        streetPosition.get(carNumber)));
+                        
+                        Thread.sleep(6000);
+                        
+                        System.out.println(String.format("Car %1s drives to"
+                            + " intersection%2s", streetPosition.get(i),
+                            streetChoice));
+                    }
+                }
+                else
+                {
+                    System.out.println(String.format("\nCar %1s drives to"
+                        + " intersection%2s", streetPosition.get(i),
+                        streetChoice));
+                }
+
+            }
+            
         }catch (InterruptedException | ExecutionException iex)
         {
             System.out.println("Failed");
         }
-        executeCarAction.shutdown();
+        shutdownThreads();
+    }
+    
         
+    private static void randomStreet()
+    {
+        for (int i = 0; i < 4; i += 1)
+        {
+            streetChoice = ThreadLocalRandom.current().nextInt(1, 5);
+
+            if (threadIndex.contains(streetChoice))
+            {
+                while (threadIndex.contains(streetChoice))
+                {
+                    streetChoice = ThreadLocalRandom.current().nextInt(1, 5);
+                }
+            }
+            else
+            {
+                threadIndex.add(streetChoice); 
+            }
+                System.out.println(String.format("Car %1s begins at"
+                    + " intersection%2s", streetPosition.get(i),
+                    streetChoice));
+
+                carInfo.put(streetPosition.get(i), streetChoice);
+        }
+    }
+    
+    private static void shutdownThreads()
+    {
         try {
-            boolean terminateStreetNavigation = executeCarAction.awaitTermination(2, TimeUnit.SECONDS);
-            boolean terminateStreetStart = streetLocation.awaitTermination(2, TimeUnit.SECONDS);
+            boolean terminateStreetStart = 
+                    streetLocation.awaitTermination(2, TimeUnit.SECONDS);
         } catch (InterruptedException ex1)
         {
             System.out.println("Did not wait 2 seconds");
         } finally
         {
-            if (!executeCarAction.isTerminated() || !streetLocation.isTerminated())
+            if (!streetLocation.isTerminated())
             {
-                List<Runnable> unfinished = executeCarAction.shutdownNow();
                 List<Runnable> streetAssignments = streetLocation.shutdownNow();
             }
         }
-                
     }
-    
 }
--- AtomicConcurrency/src/atomicmethodconcurrency/TrafficIntersectionStartingPoint.java
+++ AtomicConcurrency/src/atomicmethodconcurrency/TrafficIntersectionStartingPoint.java
@@ -1,34 +1,52 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * @author Reuben Ellis
+ * Date: 2016-08-13
+ * This class implements Callable and has a method named atomic
+ * The ArrayList contains names of the current thread so only four threads
+ * can call the atomic method
+ * The atomic method is set up where it is called concurrently unless the 
+ * thread name already exists and then the thread which exists cannot invoke
+ * atomic concurrently and gets blocked
+ * The atomic method allows all four threads to access the method, but blocks 
+ * a thread if it has already accessed the method and the method returns each
+ * of the four Thread names to the call method
+ * The call method which is an Override as a result of implementing Callable, 
+ * returns the name of the thread as an integer for use in the main class
+ * The name of the thread is passed as an int because the method substring is 
+ * used with a value of 14 to only get the numeric value of the current thread
  */
 package atomicmethodconcurrency;
+import java.util.ArrayList;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
-/**
- *
- * @author ethri
- */
+
 public class TrafficIntersectionStartingPoint implements Callable<Integer> {
     
-    public int atomic()
+    private ArrayList<String>
+            threadContainer = new ArrayList<String>();
+
+    public String atomic()
     {
-        int streetChoice = ThreadLocalRandom.current().nextInt(1, 5);
+        String
+                threadInstance = Thread.currentThread().getName().substring(14);
         
-        for (int i = 1; i <= streetChoice; i++)
+        if (!threadContainer.contains(String.format("Car %1s", threadInstance)))
         {
-            System.out.println("Car " + 
-                Thread.currentThread().getName().substring(14) + 
-                " begins at lane: " + i);
+            threadContainer.add(String.format("Car %1s", threadInstance));
+            return threadInstance;
         }
-        return streetChoice;
+        else
+        {
+        }
+        return threadInstance;
     }
     
     @Override
     public Integer call()
     {
-        atomic();
-        return atomic();
+        String
+                threadNumber = "";
+        threadNumber = atomic();
+        return Integer.parseInt(threadNumber);
     }
 }
